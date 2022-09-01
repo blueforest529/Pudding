cb818febca5d13f268283bdeeaabee5dda08d827
cheong <imcheong001@gmail.com>
2022-09-01 13:39:48 +0900

    frontend 다시 복구

---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const dirname = path.resolve()
+
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ],
+  lintOnSave: false,
+  outputDir: path.resolve(dirname, '../backend/dist')
+})
